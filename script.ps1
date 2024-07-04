60b3d973a0e63c7cced50ca521b56d88eb123124
Louis Ruocco <louisruocco1@gmail.com>
2024-07-04 21:55:03 +0100

    email able to be sent, body needs work

--- script.ps1
+++ script.ps1
@@ -10,6 +10,7 @@ $arms = Get-Content ".\files\arms.txt"
 $back = Get-Content ".\files\back.txt"
 $legs = Get-Content ".\files\legs.txt"
 $abs = Get-Content ".\files\abs.txt"
+$date = Get-Date -Format "dd-MM-yyyy"
 
 $backExercises = Randomise-Exercise -exercises $back
 $armExercises = Randomise-Exercise -exercises $arms
@@ -18,3 +19,25 @@ $abExercises = Randomise-Exercise -exercises $abs
 
 $backExercises[0..1]
 ## Send Email
+$username = <redacted>
+$password = <redacted> | ConvertTo-SecureString -AsPlainText -Force
+
+$body = @"
+    <h2>Back</h2>
+    <ul>
+        <li>$backExercises</li>
+    </ul>    
+"@
+$email = @{
+    from = <redacted>
+    to = <redacted>
+    subject = "Gym Routine | $date"
+    smtpserver = "smtp.gmail.com"
+    body = $body
+    port = 587
+    credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+    usessl = $true
+    verbose = $true
+}
+
+Send-MailMessage @email -BodyAsHtml
